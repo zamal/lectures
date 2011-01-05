96e22a0faaf2040ec604d7ae9aa5ddd2469fb853
Maxim Zakharov <thezamal@gmail.com>
2011-01-05 23:32:09 +0300

    * other/SiSPI_Lectures.tex: Яндекс Метрика

---
+++ other/metrika.js
@@ -0,0 +1,22 @@
+<!-- Yandex.Metrika informer -->
+<a href="http://metrika.yandex.ru/stat/?id=3076903&amp;from=informer"
+target="_blank"><img src="//bs.yandex.ru/informer/3076903/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+width="88" height="31" alt="Яндекс.Метрика" border="0" /></a>
+<!-- /Yandex.Metrika informer -->
+
+<!-- Yandex.Metrika counter -->
+<div style="display:none;"><script type="text/javascript">
+(function(w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter3076903 = new Ya.Metrika(3076903);
+             yaCounter3076903.clickmap(true);
+             yaCounter3076903.trackLinks(true);
+        
+        } catch(e) {}
+    });
+})(window, 'yandex_metrika_callbacks');
+</script></div>
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
+<noscript><div style="position:absolute"><img src="//mc.yandex.ru/watch/3076903" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
